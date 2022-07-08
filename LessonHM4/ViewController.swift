c99b1ed05547511dbfac17eac247d9597db62c04
Dima Hubenya <dima96gubenya@gmail.com>
2022-07-08 13:45:40 +0300

    Initial Commit

---
+++ LessonHM4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonHM4
+//
+//  Created by Дима Губеня on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
